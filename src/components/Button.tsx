f641768f5a4c7633341bdcaafe145df419480506
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2025-03-22 09:40:07 +0300

    sprint 01 week 03 dop 01: callback

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+type ButtonProps = {
+  title: string
+  onClick: () => void
+}
+
+export const Button = ({title, onClick}: ButtonProps) => {
+  const onClickHandler = () => {
+    onClick()
+  }
+
+  return (
+    <button onClick={onClickHandler}>{title}</button>
+  )
+}
